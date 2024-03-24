f5aa33aa4e9180329792add64e7497290c7c3180
benjamin <benja037@gmail.com>
2024-03-24 15:46:39 -0300

    change views

--- django_crud_api/views.py
+++ django_crud_api/views.py
@@ -2,13 +2,25 @@ from rest_framework.decorators  import api_view
 from rest_framework.response import Response
 from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.models import User
 from rest_framework import status
 
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+
 @api_view(['POST'])
 def login(request):
-    return Response({})
+    user = get_object_or_404(User, username=request.data['username'])
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Invalid password"}, status = status.HTTP_400_BAD_REQUEST)
+    
+    token,created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+    
+    return Response({"token": token.key,"user": serializer.data}, status.HTTP_200_OK)
 
 
 @api_view(['POST'])
@@ -25,6 +37,15 @@ def register(request):
         return Response({'token': token.key, "user": serializer.data},status = status.HTTP_201_CREATED)
 
     return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+
+
 @api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 def profile(request):
-    return Response({})
+    print(request.user)
+
+
+
+    return Response("You are login with {}".format(request.user.username),status = status.HTTP_200_OK)
