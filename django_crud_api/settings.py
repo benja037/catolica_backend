0edca4e55f6d26725334ae86b47b161ad13ddb33
benjamin <benja037@gmail.com>
2024-04-28 23:52:01 -0400

    prueba tokens 3

--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=2),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=1),
     "REFRESH_TOKEN_LIFETIME": timedelta(minutes=15),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
