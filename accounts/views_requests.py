2c96f999e8c6c8f5b23b6b7d355e763e6a7dddf3
benjamin <benja037@gmail.com>
2024-05-23 14:41:35 -0400

    Add subject request deny or accept

--- accounts/views_requests.py
+++ accounts/views_requests.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.response import Response
 from rest_framework import status
@@ -10,49 +11,7 @@ from .models import Student,Discipline, Subject, Teacher, CustomUser,StudentSubj
 from rest_framework.permissions import IsAuthenticated
 
 
-""" @permission_classes([IsAuthenticated])
-class Requests_Create(ModelViewSet):    
-    serializer_class = DisciplineSerializer       
-    queryset = Discipline.objects.all()
-    def get_queryset(self):        
-        all_disciplines = Discipline.objects.all()       
-        serializer = self.serializer_class(all_disciplines, many=True)
-        return Response(serializer.data)
-    
-    def list_my_requests(self,request):
-        try:
-            all_disciplines = Discipline.objects.all()
-            serializer = DisciplineSerializer(all_disciplines, many=True)
-            return Response(serializer.data)
-        except Discipline.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
 
-        
-    @action(detail=True, methods=['get'])    
-    def retrieve_request(self, request,discipline_pk=None):
-        try:
-            discipline = Discipline.objects.get(id=discipline_pk)
-            serializer = DisciplineSerializer(discipline)
-            return Response(serializer.data)
-        except Discipline.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-   
-    @action(detail=False, methods=['post'])
-    def create_auto_request(self, request):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    @action(detail=True, methods=['delete'])
-    def delete_discipline(self, request,discipline_pk=None):
-        try:
-            discipline = Discipline.objects.get(id=discipline_pk)
-            discipline.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
-        except Discipline.DoesNotExist:
-            return Response(status=status.HTTP_404_NOT_FOUND) """
         
     
 
@@ -86,3 +45,20 @@ class Requests_GetPatch(ModelViewSet):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except StudentSubjectRequest.DoesNotExist:
             return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    @action(detail=True, methods=['patch'])
+    def acceptordeny_subject_request(self, request,subject_pk=None,request_pk=None):        
+        subject = get_object_or_404(Subject,id=subject_pk)
+        studentsubjectrequest = get_object_or_404(StudentSubjectRequest,id=request_pk)
+        accept = request.data.get('accept')
+        if accept is not None:
+            if accept == True:
+                subject.students.add(studentsubjectrequest.student)
+                studentsubjectrequest.state = 'aceptado'
+            elif accept == False:
+                studentsubjectrequest.state = 'rechazado'
+            studentsubjectrequest.save()
+            return Response({'status': 'Solicitud actualizada'}, status=status.HTTP_200_OK)
+        else:
+            return Response({'error': 'El campo "accept" es requerido'}, status=status.HTTP_400_BAD_REQUEST)
+
