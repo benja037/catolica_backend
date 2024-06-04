f15e11fd2a4a8a2f82d6d8292f8e129e45d496a8
benjamin <benja037@gmail.com>
2024-06-04 18:09:23 -0400

    Add url profile customuser

---
+++ accounts/views_customuser.py
@@ -0,0 +1,21 @@
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import action,permission_classes
+from accounts.serializers import SimpleStudentSerializer, StudentSerializer, UserSerializer
+
+from .models import CustomUser
+from rest_framework.permissions import IsAuthenticated
+
+#List [ID,subject_name,staff_id] /subjectss/
+@permission_classes([IsAuthenticated])
+class CustomUser_allView(ModelViewSet):    
+    serializer_class = UserSerializer      
+    def retrieve_customuser(self,request):
+        try:
+            customuser = CustomUser.objects.get(id=request.user.id)
+            
+            serializer = UserSerializer(customuser, many=True)
+            return Response(serializer.data)
+        except CustomUser.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
